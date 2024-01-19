0b436b4dda4d171d2e9a0875b93e76e5f9b14b57
SardorTashmatovs <tashmatovs35@gmail.com>
2024-01-19 21:43:51 +0500

    first commit

---
+++ p.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, creator, year):
+        self.title = title
+        self.creator = creator
+        self.year = year
+
+    def info(self):
+        print(f"{self.title}")
+        print(f"{self.creator}")
+        print(f"{self.year}")
+
+book1 = Book("a", "b", 1999)
+
+book1.info()
+
+
